d0dca811487225364e1876c8bbc6e32b2d87bd4c
Pedro Henrique Azevedo <phazevedo28@gmail.com>
2022-09-11 17:20:20 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import icon from '../../assets/img/Vector.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
-    return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
-        </div>
-    )
+type Props = {
+    saleId: number;
 }
 
-export default NotificationButton
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("SUCESSO");
+        })
+}
+
+
+function NotificationButton({ saleId }: Props) {
+    return (
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)} >
+            <img src={icon} alt="Notificar" />
+        </ div>
+            )
+}
+
+            export default NotificationButton
+
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,7 +21,6 @@ function SalesCard() {
 
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
-        console.log(minDate);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
@@ -78,7 +77,7 @@ function SalesCard() {
                                         <td>
                                             <div className="dsmeta-red-btn-container">
                                                 <div className="dsmeta-red-btn">
-                                                    <NotificationButton />
+                                                    <NotificationButton saleId={sale.id} />
                                                 </div>
                                             </div>
                                         </td>
